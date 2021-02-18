ef74855c707f338c59c2db3bdb12fb5128f387c2
Kozyava <48145809+25081998@users.noreply.github.com>
2021-02-18 16:02:51 +0300

    Add files via upload

---
+++ Command/Command/Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace Command
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBoxA2 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.textBoxA1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip3 = new System.Windows.Forms.MenuStrip();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.Назад = new System.Windows.Forms.ToolStripButton();
+            this.Повторить = new System.Windows.Forms.ToolStripButton();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBoxRes = new System.Windows.Forms.TextBox();
+            this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сложениеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.вычитаниеToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.умножениеToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.делениеToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBoxB1 = new System.Windows.Forms.TextBox();
+            this.textBoxB2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.menuStrip3.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBoxB2);
+            this.groupBox1.Controls.Add(this.textBoxB1);
+            this.groupBox1.Controls.Add(this.textBoxA2);
+            this.groupBox1.Controls.Add(this.menuStrip1);
+            this.groupBox1.Controls.Add(this.textBoxA1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 72);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(470, 227);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ввод данных";
+            // 
+            // textBoxA2
+            // 
+            this.textBoxA2.Location = new System.Drawing.Point(146, 156);
+            this.textBoxA2.Name = "textBoxA2";
+            this.textBoxA2.Size = new System.Drawing.Size(29, 27);
+            this.textBoxA2.TabIndex = 13;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.сложениеToolStripMenuItem,
+            this.вычитаниеToolStripMenuItem1,
+            this.умножениеToolStripMenuItem1,
+            this.делениеToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(3, 23);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(464, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // textBoxA1
+            // 
+            this.textBoxA1.Location = new System.Drawing.Point(146, 87);
+            this.textBoxA1.Name = "textBoxA1";
+            this.textBoxA1.Size = new System.Drawing.Size(29, 27);
+            this.textBoxA1.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Первое число:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Второе число:";
+            // 
+            // menuStrip3
+            // 
+            this.menuStrip3.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip3.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.справкаToolStripMenuItem});
+            this.menuStrip3.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip3.Name = "menuStrip3";
+            this.menuStrip3.Size = new System.Drawing.Size(753, 28);
+            this.menuStrip3.TabIndex = 12;
+            this.menuStrip3.Text = "menuStrip3";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Назад,
+            this.Повторить});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 28);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(753, 27);
+            this.toolStrip1.TabIndex = 15;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // Назад
+            // 
+            this.Назад.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Назад.Image = global::Command.Properties.Resources.agt_back_256;
+            this.Назад.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Назад.Name = "Назад";
+            this.Назад.Size = new System.Drawing.Size(24, 24);
+            this.Назад.Text = "Назад";
+            this.Назад.Click += new System.EventHandler(this.Назад_Click);
+            // 
+            // Повторить
+            // 
+            this.Повторить.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Повторить.Image = global::Command.Properties.Resources.View_refresh_Gion_simple_svg;
+            this.Повторить.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Повторить.Name = "Повторить";
+            this.Повторить.Size = new System.Drawing.Size(24, 24);
+            this.Повторить.Text = "Повторить";
+            this.Повторить.Click += new System.EventHandler(this.Повторить_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBoxRes);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox3.Location = new System.Drawing.Point(488, 72);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(252, 227);
+            this.groupBox3.TabIndex = 17;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Результат";
+            // 
+            // textBoxRes
+            // 
+            this.textBoxRes.Location = new System.Drawing.Point(56, 101);
+            this.textBoxRes.Name = "textBoxRes";
+            this.textBoxRes.Size = new System.Drawing.Size(145, 27);
+            this.textBoxRes.TabIndex = 12;
+            // 
+            // справкаToolStripMenuItem
+            // 
+            this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            this.справкаToolStripMenuItem.Size = new System.Drawing.Size(79, 24);
+            this.справкаToolStripMenuItem.Text = "Справка";
+            this.справкаToolStripMenuItem.Click += new System.EventHandler(this.справкаToolStripMenuItem_Click);
+            // 
+            // сложениеToolStripMenuItem
+            // 
+            this.сложениеToolStripMenuItem.Name = "сложениеToolStripMenuItem";
+            this.сложениеToolStripMenuItem.Size = new System.Drawing.Size(92, 24);
+            this.сложениеToolStripMenuItem.Text = "Сложение";
+            this.сложениеToolStripMenuItem.Click += new System.EventHandler(this.сложениеToolStripMenuItem_Click);
+            // 
+            // вычитаниеToolStripMenuItem1
+            // 
+            this.вычитаниеToolStripMenuItem1.Name = "вычитаниеToolStripMenuItem1";
+            this.вычитаниеToolStripMenuItem1.Size = new System.Drawing.Size(98, 24);
+            this.вычитаниеToolStripMenuItem1.Text = "Вычитание";
+            this.вычитаниеToolStripMenuItem1.Click += new System.EventHandler(this.вычитаниеToolStripMenuItem1_Click);
+            // 
+            // умножениеToolStripMenuItem1
+            // 
+            this.умножениеToolStripMenuItem1.Name = "умножениеToolStripMenuItem1";
+            this.умножениеToolStripMenuItem1.Size = new System.Drawing.Size(104, 24);
+            this.умножениеToolStripMenuItem1.Text = "Умножение";
+            this.умножениеToolStripMenuItem1.Click += new System.EventHandler(this.умножениеToolStripMenuItem1_Click);
+            // 
+            // делениеToolStripMenuItem1
+            // 
+            this.делениеToolStripMenuItem1.Name = "делениеToolStripMenuItem1";
+            this.делениеToolStripMenuItem1.Size = new System.Drawing.Size(81, 24);
+            this.делениеToolStripMenuItem1.Text = "Деление";
+            this.делениеToolStripMenuItem1.Click += new System.EventHandler(this.делениеToolStripMenuItem1_Click);
+            // 
+            // textBoxB1
+            // 
+            this.textBoxB1.Location = new System.Drawing.Point(206, 87);
+            this.textBoxB1.Name = "textBoxB1";
+            this.textBoxB1.Size = new System.Drawing.Size(29, 27);
+            this.textBoxB1.TabIndex = 16;
+            // 
+            // textBoxB2
+            // 
+            this.textBoxB2.Location = new System.Drawing.Point(206, 156);
+            this.textBoxB2.Name = "textBoxB2";
+            this.textBoxB2.Size = new System.Drawing.Size(29, 27);
+            this.textBoxB2.TabIndex = 17;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(241, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 20);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "i";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(241, 159);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 20);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "i";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(181, 90);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(19, 20);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "+";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(181, 159);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(19, 20);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "+";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 314);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip3);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Комплексный калькулятор";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.menuStrip3.ResumeLayout(false);
+            this.menuStrip3.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBoxA2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.TextBox textBoxA1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip3;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton Назад;
+        private System.Windows.Forms.ToolStripButton Повторить;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBoxRes;
+        private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сложениеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem вычитаниеToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem умножениеToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem делениеToolStripMenuItem1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxB2;
+        private System.Windows.Forms.TextBox textBoxB1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Command/Command/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Command
+{
+    //вызывающий
+    public partial class Form1 : Form
+    {
+        private List<Command> commands = new List<Command>();
+        private Complex result;
+        private int current = 0;
+        //конструктор
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //справка
+        private void справкаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Комплексное число — это выражение вида a + bi, где a, b — действительные числа, а i — так называемая мнимая единица, символ, квадрат которого равен –1, то есть i^2 = –1.\n\n" +
+                            "Арифметические действия над комплексными числами те же, что и над действительными: их можно складывать, вычитать, умножать и делить друг на друга.\n\n" +
+                            "Для использования калькулятора введите все данные (действительные и мнимые части интересующих чисел, не забывайте про знаки). Если какая-либо часть комплексного числа равна нулю, то введите 0. Не забывайте про существование коэффициента 1!!!\n\n" +
+                            "Разработчик: Бабичева Анна М8О-202Б-17 2018г.");
+        }
+        //печать результата
+        public void Print() { textBoxRes.Text = result.Print(); }
+        //функция повторить
+        public void Redo(int levels)
+        {
+            for (int i = 0; i < levels; i++)
+                if (current < commands.Count)
+                {
+                    result = commands[current++].Execute();
+                    Print();
+                }
+        }
+        //функция отменить
+        public void Undo(int levels)
+        {
+            for (int i = 0; i < levels; i++)
+                if (current > 0)
+                {
+                    result = commands[--current].UnExecute();
+                    Print();
+                }
+        }
+        //функция подсчета
+        public void Compute(Command command)
+        {
+            result = command.Execute();
+            commands.Add(command);
+            current++;
+            Print();
+        }
+
+        private void сложениеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Command command = new SumCommand(new Complex(Convert.ToDouble(textBoxA1.Text), Convert.ToDouble(textBoxB1.Text)), 
+                                             new Complex(Convert.ToDouble(textBoxA2.Text), Convert.ToDouble(textBoxB2.Text)));
+            Compute(command);
+        }
+
+        private void вычитаниеToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Command command = new SubtractionCommand(new Complex(Convert.ToDouble(textBoxA1.Text), Convert.ToDouble(textBoxB1.Text)),
+                                             new Complex(Convert.ToDouble(textBoxA2.Text), Convert.ToDouble(textBoxB2.Text)));
+            Compute(command);
+        }
+
+        private void умножениеToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Command command = new MultiplicationCommand(new Complex(Convert.ToDouble(textBoxA1.Text), Convert.ToDouble(textBoxB1.Text)),
+                                             new Complex(Convert.ToDouble(textBoxA2.Text), Convert.ToDouble(textBoxB2.Text)));
+            Compute(command);
+        }
+
+        private void делениеToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Command command = new SegmentationCommand(new Complex(Convert.ToDouble(textBoxA1.Text), Convert.ToDouble(textBoxB1.Text)),
+                                             new Complex(Convert.ToDouble(textBoxA2.Text), Convert.ToDouble(textBoxB2.Text)));
+            Compute(command);
+        }
+
+        private void Назад_Click(object sender, EventArgs e)
+        {
+            Undo(1);
+        }
+
+        private void Повторить_Click(object sender, EventArgs e)
+        {
+            Redo(1);
+        }
+    }
+}
